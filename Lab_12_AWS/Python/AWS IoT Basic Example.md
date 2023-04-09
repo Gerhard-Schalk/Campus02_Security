1cf73f2fb57dd1e84f572d3f3bf07dc525471e69
Gerhard Schalk <81297606+Gerhard-Schalk@users.noreply.github.com>
2023-04-09 23:22:21 +0200

    Add files via upload

---
+++ Lab_12_AWS/Python/AWS IoT Basic Example.md
@@ -0,0 +1,62 @@
+#Create AWS credentials (keys and certificates)
+
+Tools - download and installation
+OpenSSL Windows Binary: https://slproweb.com/products/Win32OpenSSL.html
+OpenSSL MAC installation guide https://macappstore.org/openssl/
+OpenSSL Linux installation:
+```
+sudo apt install openssl
+```
+
+MQTT Explorer (portable): http://mqtt-explorer.com/
+
+
+```
+Generate a client RSA key
+```
+openssl genrsa -out aws_demo_device.key 2048
+```
+Now we can check the private RSA Key
+
+```
+openssl rsa -in aws_demo_device.key -check
+```
+
+To view the key's details, you can use the following OpenSSL command:
+
+```
+openssl rsa -in aws_demo_device.key -noout -text
+```
+
+Next, we create a signing request file for the for private RSA key
+
+(Wikipedia https://de.wikipedia.org/wiki/Certificate_Signing_Request)
+
+**Note:**  The Common Name (CN) needs to match the hostname of your broker!
+          For your example we need to use **"10.0.0.1"**.
+```
+openssl req -out aws_demo_device.csr -key aws_demo_device.key -new
+```
+```
+Use the following OpenSSL command to check and print the the Certificate Signing Request (CSR) in text form:
+
+```
+openssl req -text -verify -in aws_demo_device.csr -noout
+```
+
+Now we can upload our Certificate Signing Request (csr) file to AWS IoT and create the device certificate.
+
+Use OpenSSL to print the device certificate in text form:
+
+```
+openssl x509 -in aws_demo_device.crt -text -noout
+```
+Now you can use the AWS credentials (keys and certificates) to run the aws_basicPubSub.py example to connect to AWS via MQTT and TLS. 
+
+Task: Use Wireshark to analyze the connection.
+
+Wireshark filter setting:
+
+```
+tcp.port == 8883 && tls
+```
---
+++ Lab_12_AWS/Python/aws_basicPubSub.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+# AWS IoT Device SDK for Python Example
+# https://github.com/aws/aws-iot-device-sdk-python
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import os
+import logging
+import time
+import json
+
+# Custom MQTT message callback
+def customCallback(client, userdata, message):
+    print("Received a new message: ")
+    print(message.payload)
+    print("from topic: ")
+    print(message.topic)
+    print("--------------\n\n")
+
+
+# Configure your device and security credentials
+host = 'a368ann75737xj-ats.iot.eu-central-1.amazonaws.com' # Enter your device data endpoint (see AWS IoT > Settings)
+port = 8883 # MQTT Port default 8883
+
+rootCAPath = 'AmazonRootCA1.pem'
+certificatePath = 'aws_demo_device.pem.crt'
+privateKeyPath = 'aws_demo_device.key'
+
+clientId = 'Python_Device' # Your Things Name
+
+topic = 'Test' # MQTT topic
+
+
+# Configure logging
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+# Init AWSIoTMQTTClient
+myAWSIoTMQTTClient = None
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureEndpoint(host, port)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+# Connect and subscribe to AWS IoT
+myAWSIoTMQTTClient.connect()
+myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
+time.sleep(2)
+
+# Publish to the same topic in a loop forever
+loopCount = 0
+while True:
+    message = {}
+    message['message'] = 'Hello Word : '
+    message['sequence'] = loopCount
+    messageJson = json.dumps(message)
+    myAWSIoTMQTTClient.publish(topic, messageJson, 1)
+    print('Published topic %s: %s\n' % (topic, messageJson))
+    loopCount += 1
+    time.sleep(5)
